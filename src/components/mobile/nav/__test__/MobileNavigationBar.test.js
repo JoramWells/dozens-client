015a71f6c8ba295f96e8adb9ac63e9c20b8d5221
bramuel <jorammanoah1@gmail.com>
2021-06-09 00:57:34 -0700

    Added snapshots

--- src/components/mobile/nav/__test__/MobileNavigationBar.test.js
+++ src/components/mobile/nav/__test__/MobileNavigationBar.test.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {isTSAnyKeyword} from '@babel/types'
-import MobileNavigationBar from '../MobileNavigationBar'
-import {render} from '@testing-library/react'
-import "jest-dom/extend-expect"
+import React from "react";
+import ReactDOM from "react-dom";
+import { isTSAnyKeyword } from "@babel/types";
+import MobileNavigationBar from "../MobileNavigationBar";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import renderer from "react-test-renderer"
 
-it("renders without crashing", ()=>{
-    const nav = document.createElement("div");
-    ReactDOM.render(<MobileNavigationBar/>,nav)
-})
+afterEach(cleanup)
+it("renders without crashing", () => {
+  const nav = document.createElement("div");
+  ReactDOM.render(<MobileNavigationBar />, nav);
+});
 
-it("renders Navbar correctly",()=>{
-    const {getByTestId} = render(<MobileNavigationBar home="jay" />)
-    expect(getByTestId('mobile_nav')).toHaveTextContent("")
-    
+it("renders Navbar correctly", () => {
+  const { getByTestId } = render(<MobileNavigationBar home="jay" />);
+  expect(getByTestId("mobile_nav")).toHaveTextContent("jay");
+});
+it("renders Navbar correctly", () => {
+    const { getByTestId } = render(<MobileNavigationBar home="bram" />);
+    expect(getByTestId("mobile_nav")).toHaveTextContent("bram");
+  });
 
-})
+  it("matches snapshot",()=>{
+      const tree = renderer.create(<MobileNavigationBar home="home" />).toJSON()
+      expect(tree).toMatchSnapshot()
+  })
