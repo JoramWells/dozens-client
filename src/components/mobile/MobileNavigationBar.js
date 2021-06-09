f7eb585c1a2f11cab9dbb24b739d595bed488131
bramuel <jorammanoah1@gmail.com>
2021-06-08 22:00:44 -0700

    Added transition

--- src/components/mobile/MobileNavigationBar.js
+++ src/components/mobile/MobileNavigationBar.js
@@ -1,46 +1,54 @@
 import React, { useCallback, useState } from "react";
-import { ArrowNarrowLeftIcon } from "@heroicons/react/solid";
+import { ArrowNarrowLeftIcon, HomeIcon, SearchIcon, UserIcon, XIcon } from "@heroicons/react/solid";
 
 function MobileNavigationBar() {
   const [visible, setVisible] = useState("hidden");
   const [diVisible, setDiVisible] = useState("visible");
 
-  function showNav(){
+  const showNav=useCallback(()=>{
     setVisible("visible");
     setDiVisible("hidden");
 
-  }
+  },[setVisible,setDiVisible])
 
   const hideNav = useCallback(() => {
     setVisible("hidden");
     setDiVisible("visible");
-  }, [setDiVisible]);
+  }, [setDiVisible,setVisible]);
 
   return (
     <>
       {/* Search nav */}
       <nav
-        style={{ visibility: visible }}
+        style={{ visibility: visible, transition:"350ms" }}
         className="fixed top-0 w-full shadow-md p-2 bg-yellow-400 z-0"
       >
         <div className="flex flex-row justify-center content-center items-center space-x-4">
           <div onClick={()=>hideNav()}>
-            <ArrowNarrowLeftIcon className="h-5" />
+            <ArrowNarrowLeftIcon className="h-5 text-gray-700" />
           </div>
-          <div className="">
+          <div className=" bg-yellow-200 flex flex-row items-center content-center rounded-full">
+              <SearchIcon className="h-5 m-1 text-gray-500" />
             <input placeholder="Search..." className=" bg-transparent p-1" />
+            <XIcon className="h-5 m-1 text-gray-500" />
           </div>
         </div>
       </nav>
 
       {/* Home nav */}
       <nav
-        className="flex flex-row p-2 justify-between shadow-md bg-yellow-400"
-        style={{ visibility: diVisible }}
+        className="flex flex-row p-3 justify-between shadow-md bg-yellow-400"
+        style={{ visibility: diVisible, transition:"350ms" }}
       >
-        <button onClick={showNav}>Home</button>
-        <div>Search</div>
-        <div>Account</div>
+          <div>
+              <HomeIcon className="h-5 text-gray-700" />
+          </div>
+        <div>
+            <SearchIcon className="h-5 text-gray-700" onClick={showNav} />
+        </div>
+        <div>
+            <UserIcon className="h-5 text-gray-700" />
+        </div>
       </nav>
     </>
   );
