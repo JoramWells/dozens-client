0835d8c8d5817e40402ac0eec8ad0b660f38c01e
bramuel <jorammanoah1@gmail.com>
2021-06-08 19:53:58 -0700

    Added mobile navigation

---
+++ src/components/mobile/MobileNavigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MobileNavigationBar() {
+    return (
+        <nav>
+            
+            
+        </nav>
+    )
+}
+
+export default MobileNavigationBar
