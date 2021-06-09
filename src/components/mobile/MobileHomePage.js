8023b5a997810358e75d2d277a4523ba75aba972
bramuel <jorammanoah1@gmail.com>
2021-06-08 23:28:12 -0700

    Added more image

--- src/components/mobile/MobileHomePage.js
+++ src/components/mobile/MobileHomePage.js
@@ -74,6 +74,90 @@ export default function MobileHomePage() {
               </figcaption>
             </div>
           </figure>
+          <figure style={{width:"18rem", border:"1px solid whitesmoke"}}>
+            <img
+              src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+              style={{ width: "18rem", height: "170px" }}
+              alt="shoe3"
+            />
+            <div className="p-2">
+              <blockquote>product name</blockquote>
+              <figcaption>
+                <div>
+                  <p className="font-semibold text-gray-600">3,456/=</p>
+                </div>
+                <div className="line-through text-sm text-gray-500">
+                  12,344/=
+                </div>
+                <div>
+                  <ThumbUpIcon className="h-5 text-gray-500" />
+                </div>
+              </figcaption>
+            </div>
+          </figure>
+          <figure style={{width:"18rem", border:"1px solid whitesmoke"}}>
+            <img
+              src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+              style={{ width: "18rem", height: "170px" }}
+              alt="shoe3"
+            />
+            <div className="p-2">
+              <blockquote>product name</blockquote>
+              <figcaption>
+                <div>
+                  <p className="font-semibold text-gray-600">3,456/=</p>
+                </div>
+                <div className="line-through text-sm text-gray-500">
+                  12,344/=
+                </div>
+                <div>
+                  <ThumbUpIcon className="h-5 text-gray-500" />
+                </div>
+              </figcaption>
+            </div>
+          </figure>
+          <figure style={{width:"18rem", border:"1px solid whitesmoke"}}>
+            <img
+              src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+              style={{ width: "18rem", height: "170px" }}
+              alt="shoe3"
+            />
+            <div className="p-2">
+              <blockquote>product name</blockquote>
+              <figcaption>
+                <div>
+                  <p className="font-semibold text-gray-600">3,456/=</p>
+                </div>
+                <div className="line-through text-sm text-gray-500">
+                  12,344/=
+                </div>
+                <div>
+                  <ThumbUpIcon className="h-5 text-gray-500" />
+                </div>
+              </figcaption>
+            </div>
+          </figure>
+          <figure style={{width:"18rem", border:"1px solid whitesmoke"}}>
+            <img
+              src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+              style={{ width: "18rem", height: "170px" }}
+              alt="shoe3"
+            />
+            <div className="p-2">
+              <blockquote>product name</blockquote>
+              <figcaption>
+                <div>
+                  <p className="font-semibold text-gray-600">3,456/=</p>
+                </div>
+                <div className="line-through text-sm text-gray-500">
+                  12,344/=
+                </div>
+                <div>
+                  <ThumbUpIcon className="h-5 text-gray-500" />
+                </div>
+              </figcaption>
+            </div>
+          </figure>
         </div>
       </main>
     </div>
