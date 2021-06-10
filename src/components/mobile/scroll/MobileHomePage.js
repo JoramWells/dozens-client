217795904be597cc4422dd0c32f619796d67b69b
bramuel <jorammanoah1@gmail.com>
2021-06-09 22:58:50 -0700

    Changed bg color

--- src/components/mobile/scroll/MobileHomePage.js
+++ src/components/mobile/scroll/MobileHomePage.js
@@ -1,4 +1,4 @@
-import { ThumbUpIcon } from "@heroicons/react/solid";
+import { DotsVerticalIcon, ThumbUpIcon } from "@heroicons/react/solid";
 import React from "react";
 import { Helmet } from "react-helmet";
 
@@ -10,9 +10,15 @@ export default function MobileHomePage({product}) {
         <title>HomePage | Dozens Kenya</title>
       </Helmet>
 
-      <div className="p-4">
+      <div className="p-4 " style={{backgroundColor:"whitesmoke"}}>
         <div className="flex flex-col content-center items-center space-y-8">
-          <figure style={{ width: "18rem", border: "1px solid #E8E8E8 " }} className="rounded-md">
+          <figure style={{ width: "18rem", border: "1px solid #F0F0F0 " }} className="rounded-md bg-white">
+            <div
+            className="absolute bg-black text-gray-200 p-1 rounded-full m-1 bg-opacity-20"
+            style={{right:"20px"}}
+            >
+              <DotsVerticalIcon className="h-5" />
+            </div>
             <img
               src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
               style={{ width: "18rem", height: "170px" }}
@@ -27,17 +33,16 @@ export default function MobileHomePage({product}) {
               <figcaption>
                 <div>
                   <p className="font-semibold text-gray-600">{price}</p>
-                  <p className="text-lg font-semibold text-yellow-400">
+                  <p className="text-lg font-semibold text-gray-700">
                   Kshs 2,213/=
 
                   </p>
                 </div>
-                <div className="line-through text-sm text-gray-300">
+                <div className="line-through text-sm text-gray-300 font-bold">
                   {discount}
                   Kshs 4,321/=
                 </div>
-                <hr className="my-2" style={{color:"#A9A9A9"}}/>
-                <div className="flex flex-row space-x-2 items-center content-center">
+                <div className="flex flex-row space-x-2 items-center content-center mt-4">
                   {likes}
                   <p className="text-sm text-gray-400">
                   231
