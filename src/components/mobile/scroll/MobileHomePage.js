393be656afdde7e0af29eb77c251fa39bb0b109c
bramuel <jorammanoah1@gmail.com>
2021-06-09 21:44:12 -0700

    Modified card

--- src/components/mobile/scroll/MobileHomePage.js
+++ src/components/mobile/scroll/MobileHomePage.js
@@ -12,7 +12,7 @@ export default function MobileHomePage({product}) {
 
       <div className="p-4">
         <div className="flex flex-col content-center items-center space-y-8">
-          <figure style={{ width: "18rem", border: "1px solid whitesmoke" }}>
+          <figure style={{ width: "18rem", border: "1px solid whitesmoke " }} className="rounded-md">
             <img
               src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
               style={{ width: "18rem", height: "170px" }}
@@ -20,15 +20,28 @@ export default function MobileHomePage({product}) {
             />
             <div className="p-2">
               <blockquote>{productName}</blockquote>
+              <p className="text-gray-400">
+              Airforce Maziwa
+
+              </p>
               <figcaption>
                 <div>
                   <p className="font-semibold text-gray-600">{price}</p>
+                  <p className="text-lg font-semibold text-gray-700">
+                  Kshs 2,213/=
+
+                  </p>
                 </div>
-                <div className="line-through text-sm text-gray-500">
+                <div className="line-through text-sm text-gray-400">
                   {discount}
+                  Kshs 4,321/=
                 </div>
-                <div className="flex flex-row space-x-2">
+                <hr className="my-2 bg-gray-100"/>
+                <div className="flex flex-row space-x-2 items-center content-center">
                   {likes}
+                  <p className="text-sm text-gray-400">
+                  231
+                  </p>
                   <ThumbUpIcon className="h-5 text-gray-500" />
                 </div>
               </figcaption>
