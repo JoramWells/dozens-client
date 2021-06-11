fc12055ac145a467f0537caf792c39182d675953
bramuel <jorammanoah1@gmail.com>
2021-06-11 16:19:24 -0700

    Added modal

---
+++ src/components/mobile/modal/ModalComponent.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import { XIcon } from "@heroicons/react/outline";
+
+const portal = document.getElementById("portal");
+
+const Background = styled.div`
+  width: 100vh;
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.1);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 20px;
+`;
+
+const ModalHeader = styled.div``;
+
+const ModalComponent = ({ isOpen, close, children }) => {
+    const contentRef = useRef()
+    useEffect(() => {
+        if(!isOpen) return
+        function listener(e){
+            if(contentRef.current.contains(e.target))return
+            close()
+        }
+        window.addEventListener('click',listener)
+        
+        return () => window.removeEventListener('click',listener)
+    }, [isOpen])
+  if (!isOpen) return null;
+  return ReactDOM.createPortal(
+    <Background>
+      <Content className="shadow-xl" ref={contentRef}>
+        <ModalHeader>
+          <XIcon className="h-5" onClick={close} />
+        </ModalHeader>
+        {children}
+      </Content>
+    </Background>,
+    portal
+  );
+};
+
+export default ModalComponent;
--- src/components/mobile/scroll/MobileHomePage.js
+++ src/components/mobile/scroll/MobileHomePage.js
@@ -1,77 +1,67 @@
 import { DotsVerticalIcon, ThumbUpIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
+import ModalComponent from "../modal/ModalComponent";
 
-
-export default function MobileHomePage({product}) {
-  const {id,alt,productName,price,discount,likes} = product
-  // let [isOpen, setIsOpen] = useState(true)
-
-  // function closeModal() {
-  //   setIsOpen(false)
-  // }
-
-  // function openModal() {
-  //   setIsOpen(true)
-  // }
+export default function MobileHomePage({ product }) {
+  const { id, alt, productName, price, discount, likes } = product;
+  const [isOpen, setOpen] = useState(false);
 
   return (
     <>
-    
+      <div className="mobile__body" data-testid={`product-${id}`}>
+        <Helmet>
+          <title>HomePage | Dozens Kenya</title>
+        </Helmet>
 
-    <div className="mobile__body" data-testid={`product-${id}`}>
-      <Helmet>
-        <title>HomePage | Dozens Kenya</title>
-      </Helmet>
-
-
-      <div className="p-4 " style={{backgroundColor:"whitesmoke"}}>
-        <div className="flex flex-col content-center items-center space-y-8">
-          <figure style={{ width: "18rem", border: "1px solid #F0F0F0 " }} className="rounded-md bg-white">
-            <div
-            className="absolute bg-black text-gray-200 p-1 rounded-full m-1 bg-opacity-20"
-            style={{right:"20px"}}
+        <div className="p-4 " style={{ backgroundColor: "whitesmoke" }}>
+          <div className="flex flex-col content-center items-center space-y-8">
+            <figure
+              style={{ width: "18rem", border: "1px solid #F0F0F0 " }}
+              className="rounded-md bg-white"
             >
-              <DotsVerticalIcon className="h-5" />
-            </div>
-            <img
-              src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
-              style={{ width: "18rem", height: "170px" }}
-              alt={alt}
-            />
-            <div className="p-2">
-              <blockquote>{productName}</blockquote>
-              <p className="text-gray-600">
-              Airforce Maziwa
-
-              </p>
-              <figcaption>
-                <div>
-                  <p className="font-semibold text-gray-600">{price}</p>
-                  <p className="text-lg font-semibold text-gray-700">
-                  Kshs 2,213/=
-
-                  </p>
-                </div>
-                <div className="line-through text-sm text-gray-300 font-bold">
-                  {discount}
-                  Kshs 4,321/=
-                </div>
-                <div className="flex flex-row space-x-2 items-center content-center mt-4">
-                  {likes}
-                  <p className="text-sm text-gray-400">
-                  231
-                  </p>
-                  <ThumbUpIcon className="h-5 text-gray-500" />
-                </div>
-              </figcaption>
-            </div>
-          </figure>
+              <div
+                className="absolute bg-black text-gray-200 p-1 rounded-full m-1 bg-opacity-20"
+                style={{ right: "20px" }}
+              >
+                <DotsVerticalIcon
+                  className="h-5"
+                  onClick={() => setOpen(true)}
+                />
+              </div>
+              <img
+                src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+                style={{ width: "18rem", height: "170px" }}
+                alt={alt}
+              />
+              <div className="p-2">
+                <blockquote>{productName}</blockquote>
+                <p className="text-gray-600">Airforce Maziwa</p>
+                <figcaption>
+                  <div>
+                    <p className="font-semibold text-gray-600">{price}</p>
+                    <p className="text-lg font-semibold text-gray-700">
+                      Kshs 2,213/=
+                    </p>
+                  </div>
+                  <div className="line-through text-sm text-gray-300 font-bold">
+                    {discount}
+                    Kshs 4,321/=
+                  </div>
+                  <div className="flex flex-row space-x-2 items-center content-center mt-4">
+                    {likes}
+                    <p className="text-sm text-gray-400">231</p>
+                    <ThumbUpIcon className="h-5 text-gray-500" />
+                  </div>
+                </figcaption>
+              </div>
+            </figure>
+          </div>
         </div>
       </div>
-      
-    </div>
-    
+      <ModalComponent isOpen={isOpen} close={() => setOpen(false)}>
+        <p>item details</p>
+      </ModalComponent>
     </>
   );
 }
