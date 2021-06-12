57cf28d280fd2b7583a285d7aab71b0312b943bd
bramuel <jorammanoah1@gmail.com>
2021-06-12 14:53:06 -0700

    Modified Navbar

--- src/components/mobile/nav/MobileNavigationBar.js
+++ src/components/mobile/nav/MobileNavigationBar.js
@@ -7,6 +7,21 @@ import {
 } from "@heroicons/react/solid";
 import { Link } from "react-router-dom";
 import NavDropdown from "../NavDropdown";
+import styled from "styled-components";
+
+const Navbar = styled.nav`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+  transition: 350ms;
+  background-color: #303030;
+  padding: 0.7rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 function MobileNavigationBar() {
   const [visible, setVisible] = useState("hidden");
@@ -24,30 +39,9 @@ function MobileNavigationBar() {
 
   return (
     <>
-      {/* Search nav */}
-      <nav
-        style={{ visibility: visible, transition: "350ms", backgroundColor:"#303030" }}
-        className="fixed top-0 w-full shadow-md p-3 z-10 test"
-      >
-        <div className="flex flex-row justify-center content-center items-center space-x-4">
-          <div onClick={() => hideNav()}>
-            <ArrowNarrowLeftIcon className="h-5 text-gray-100" />
-          </div>
-          <div className=" bg-gray-100 flex flex-row items-center content-center rounded-full">
-            <SearchIcon className="h-5 m-1 text-gray-500" />
-            <input placeholder="Search..." className=" bg-transparent p-1" style={{margin:"0px"}} />
-            <XIcon className="h-5 m-1 text-gray-500" />
-          </div>
-        </div>
-      </nav>
-
-      {/* Home nav */}
-      <nav
-        className="flex flex-row p-3 justify-between items-center shadow-lg fixed w-full top-0 z-10"
-        style={{ visibility: diVisible, transition: "350ms",backgroundColor:"#303030" }}
-        data-testid="mobile_nav"
-      >
-        <div className="flex flex-row space-x-2 p-1">
+      {/* Home page nav */}
+      <Navbar data-testid="mobile_nav" style={{ visibility: diVisible }}>
+        <div className="p-1">
           <Link to="/">
             <HomeIcon className="h-5 text-white" />
           </Link>
@@ -57,7 +51,23 @@ function MobileNavigationBar() {
 
           <NavDropdown />
         </div>
-      </nav>
+      </Navbar>
+
+      {/* Search nav */}
+      <Navbar className="test" style={{ visibility: visible }}>
+        <div onClick={() => hideNav()}>
+          <ArrowNarrowLeftIcon className="h-5 text-gray-100" />
+        </div>
+        <div className=" bg-gray-100 flex flex-row items-center content-center rounded-full">
+          <SearchIcon className="h-5 m-1 text-gray-500" />
+          <input
+            placeholder="Search..."
+            className=" bg-transparent"
+            style={{ margin: "0", padding:".19rem" }}
+          />
+          <XIcon className="h-5 m-1 text-gray-500" />
+        </div>
+      </Navbar>
     </>
   );
 }
